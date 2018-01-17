2e642b70a40fe3d09920b337d45860a802fd9206
Murali Shankar <mshankar@slac.stanford.edu>
2018-01-17 13:46:54 -0800

    force change to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(name='psdm_qs_cli',
       version='0.1',
-      description='Python client for interacting with the PCDS questionnaire',
+      description='Python client for interacting with the PCDS questionnaire.',
       url='https://github.com/slaclab/psdm_qs_cli.git',
       author='Murali Shankar',
       author_email='mshankar@slac.stanford.edu',
       license='MIT',
-      packages=['qs_cli'],
-      scripts=['qs_cli/QSGenerateExcelSpreadSheet.py', 'qs_cli/QSGenerateJSON.py',],
+      packages=['psdm_qs_cli'],
+      scripts=['psdm_qs_cli/QSGenerateExcelSpreadSheet.py', 'psdm_qs_cli/QSGenerateJSON.py',],
       zip_safe=False)
